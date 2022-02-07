31aff0a3a858a48e5bf26db3dd9e987ebc9de5e7
Tobias Andersson <tobias@qte.se>
2022-02-07 12:26:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { FaGofore } from "react-icons/fa";
+import Navbar from "./components/NavBar/Navbar";
+import Footer from "./components/Footer/Footer";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex__center">
+      <p className="p__cormorant">Österlens Glamping</p>
     </div>
   );
-}
+};
 
 export default App;
